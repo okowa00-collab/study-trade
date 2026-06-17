b623afc38027677da0a335713afe2ff81d7729f0
okowa00-collab <okowa00@gmail.com>
2026-06-17 05:18:23 +0000

    Create database directory before saveing SQLite file

--- src/save_to_sqlite.py
+++ src/save_to_sqlite.py
@@ -1,14 +1,21 @@
 # configを見てDB保存
 
+from pathlib import Path
 import sqlite3
+
 import pandas as pd
+
 from config import PROCESSED_CSV_PATH, DB_PATH, TABLE_NAME
+
 
 def main():
     df = pd.read_csv(PROCESSED_CSV_PATH)
 
+    Path(DB_PATH).parent.mkdir(parents=True, exist_ok=True)
+
     with sqlite3.connect(DB_PATH) as conn:
         df.to_sql(TABLE_NAME, conn, if_exists="replace", index=False)
+
     print("SQLite保存OK")
 
 
